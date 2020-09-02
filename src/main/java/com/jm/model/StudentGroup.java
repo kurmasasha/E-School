881070b24dad74f56d2ba3716a6de21c5dc6bb3f
sovremennik <specialforvit@gmail.com>
2020-09-02 18:28:35 +0300

    Create entities

---
+++ src/main/java/com/jm/model/StudentGroup.java
@@ -0,0 +1,57 @@
+package com.jm.model;
+
+import javax.persistence.*;
+
+@Entity
+public class StudentGroup {
+
+    @Id
+    @Column(name = "id_student_group")
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "id_course")
+    private Course course;
+
+    @ManyToOne
+    @JoinColumn(name = "id_user")
+    private Teacher teacher;
+
+    public StudentGroup() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+}
---
+++ src/main/java/com/jm/model/Task.java
@@ -0,0 +1,71 @@
+package com.jm.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Task {
+
+    @Id
+    @Column(name = "id_task")
+    private Long id;
+
+    private String type;
+    private String title;
+    private String description;
+    private Integer points;
+    private Boolean isAvailable;
+
+    public Task() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPoints() {
+        return points;
+    }
+
+    public void setPoints(Integer points) {
+        this.points = points;
+    }
+
+    public Boolean getAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(Boolean available) {
+        isAvailable = available;
+    }
+}
